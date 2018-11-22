3e5a205c26805829f042d6f4592037f5e463e9bd
Marco Baroncini <spartaco4404@yahoo.it>
2018-11-22 22:10:56 +0100

    mb: new admin posts columns feature2

--- pluggable/duplicable/admin/columns/WebMapp_WpColumns.php
+++ pluggable/duplicable/admin/columns/WebMapp_WpColumns.php
@@ -13,7 +13,15 @@ function wm_wp_col_test( $post_id )
     echo $post_id;
 }
 
-
-new WebMapp_WpBackendColumns('poi' , "Intestazione POI" ,'wm_wp_col_test');
-new WebMapp_WpBackendColumns('route' , "Intestazione ROUTE" ,'wm_wp_col_test');
-new WebMapp_WpBackendColumns('track' , "Intestazione TRACK" ,'wm_wp_col_test');
+/**
+ * Insert:
+ *
+ * post_type (string) - For which post type do you want the column available?
+ * column header (string) - Title and identifier of a column
+ * callback name (string) - Callback for each row of table ( post id is provided as param )
+ *
+ * ENJOY
+ */
+new WebMapp_WpBackendColumns('poi' , "Intestazione POI test" ,'wm_wp_col_test');
+new WebMapp_WpBackendColumns('route' , "Intestazione ROUTE test" ,'wm_wp_col_test');
+new WebMapp_WpBackendColumns('track' , "Intestazione TRACK test" ,'wm_wp_col_test');
