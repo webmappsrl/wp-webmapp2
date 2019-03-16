5d98aa2ce5705790f47ae21041f7d78f34dc44d2
piccioli <piccioli@netseven.it>
2019-03-16 18:30:45 +0100

    Aggiunta REST API per lista lingue (actual + others)

---
+++ pluggable/duplicable/rest_routes/WebMapp_WPMLList.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Check user credentials and return user data
+ *
+ * @return object|null bounding box webmapp setting or null if none.
+ *
+ * wm_api_voucher()
+ */
+function WebMapp_V1WPMLList( WP_REST_Request $request ) {
+  $ls = apply_filters( 'wpml_active_languages', NULL, 'skip_missing=0&orderby=code' );
+  $active = '';
+  $others = array();
+  foreach ($ls as $code => $info) {
+    if($info['active']==1) {
+      $active=$code;
+    }
+    else {
+      $others[]=$code;
+    }
+  }
+  $resp['active']=$active;
+  $resp['others']=$others;
+  return new WP_REST_Response($resp,200);
+
+}
+$namespace = 'webmapp/v1';
+$route = '/wpml/list';
+$args = array(
+    'methods' => 'GET',
+    'callback' => 'WebMapp_V1WPMLList'
+);
+$WebMapp_V1WPMLList = new WebMapp_RegisterRestRoute( $namespace , $route, $args );
+
+
