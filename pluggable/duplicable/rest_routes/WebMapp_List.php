5024e75c152bfa0111b9740eb2f0cba32da5eb7a
piccioli <piccioli@netseven.it>
2019-04-13 10:39:15 +0200

    Aggiunta REST API per lista di aggiornamento dei content types json con id=>last_modified

---
+++ pluggable/duplicable/rest_routes/WebMapp_List.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Check user credentials and return user data
+ *
+ * @return object|null bounding box webmapp setting or null if none.
+ *
+ * wm_api_voucher()
+ */
+function WebMapp_V1List( WP_REST_Request $request ) {
+  $resp = array('123' => '2019-03-29T14:32:35','456'=>'2019-03-29T14:32:35');
+  return new WP_REST_Response($resp,200);
+
+}
+$namespace = 'webmapp/v1';
+$route = '/list';
+$args = array(
+    'methods' => 'GET',
+    'callback' => 'WebMapp_V1List'
+);
+$WebMapp_V1List = new WebMapp_RegisterRestRoute( $namespace , $route, $args );
+
+
