08d50132542c45fb818718bdb71f7856816f3629
Luca Baroncini <baroncini@netseven.it>
2019-09-12 14:08:53 +0200

    Change in login

--- pluggable/duplicable/assets_enqueuer/login/WebMapp_Commons.php
+++ pluggable/duplicable/assets_enqueuer/login/WebMapp_Commons.php
@@ -12,7 +12,15 @@ $WebMapp_LoginAssets = new WebMapp_AssetEnqueuer( $args,'login','style' );
 $args = array(
     'login_js' => array(
         'src' => WebMapp_ASSETS . 'js/webmapp-login.js',
-        'deps' => false
-    )
+        'deps' => false,
+        'localize' => array(
+	        'object_name' => 'webmapp_login_text',
+	        'data' => array(
+                'check_email' => __('Check the email, we sent you the link to use to reset the password', 'webmapp'),
+                'success_password' => __('The password has been reset.','webmapp'),
+	        ),
+        )
+    ),
+
 );
 $WebMapp_LoginAssets = new WebMapp_AssetEnqueuer( $args,'login','script' );
