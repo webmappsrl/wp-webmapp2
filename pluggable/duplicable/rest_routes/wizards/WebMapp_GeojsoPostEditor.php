f2fc8114f7de0540c2117a68906ecc9699c13dfb
Marco Baroncini <spartaco4404@yahoo.it>
2020-03-12 17:56:44 +0100

    updated geojson wizard

--- pluggable/duplicable/rest_routes/wizards/WebMapp_GeojsoPostEditor.php
+++ pluggable/duplicable/rest_routes/wizards/WebMapp_GeojsoPostEditor.php
@@ -26,7 +26,7 @@ function WebMapp_V3FirstWizardCallback(WP_REST_Request $request)
 
     if ( $update_id )
     {
-        $post_type = get_post_type($update_id);
+        $param["post_type"] = get_post_type($update_id);
     }
 
     $post_type = isset($param["post_type"]) ? $param["post_type"] : false;
