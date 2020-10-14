b48af2196727118681241314103267941e6c77ff
Pedram Katanchi <pedramkat@gmail.com>
2020-10-14 12:53:23 +0200

    Added hoqu job on poi save3

--- pluggable/WebMapp_HoquHook.php
+++ pluggable/WebMapp_HoquHook.php
@@ -1,43 +1,43 @@
 <?php
 
-add_action( "save_post_poi", "update_poi_job_hoqu", 10, 3);
-function update_poi_job_hoqu( $post_id, $post, $update ){
+// add_action( "save_post_poi", "update_poi_job_hoqu", 10, 3);
+// function update_poi_job_hoqu( $post_id, $post, $update ){
 
-    $site_url = site_url();
-    $hoqu_token = get_option("webmapp_hoqu_token");
-    if ($hoqu_token) {
+//     $site_url = site_url();
+//     $hoqu_token = get_option("webmapp_hoqu_token");
+//     if ($hoqu_token) {
 
-        $CURLOPT_POSTFIELDS_ARRAY = "{
-            \"instance\": \"$site_url\",
-            \"job\": \"mptupdate\",
-            \"parameters\": {
-                \"id\": \"$post_id\",
-            }
-          }";
+//         $CURLOPT_POSTFIELDS_ARRAY = "{
+//             \"instance\": \"$site_url\",
+//             \"job\": \"mptupdate\",
+//             \"parameters\": {
+//                 \"id\": \"$post_id\",
+//             }
+//           }";
         
-          $curl = curl_init();
+//           $curl = curl_init();
         
-          curl_setopt_array($curl, array(
-            CURLOPT_URL => "https://hoqustaging.webmapp.it/api/store",
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 30,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "POST",
-            CURLOPT_POSTFIELDS => $CURLOPT_POSTFIELDS_ARRAY,
-            CURLOPT_HTTPHEADER => array(
-              "Accept: application/json",
-              "Content-type: application/json",
-              "Authorization : Bearer $hoqu_token"
-            ),
-          ));
+//           curl_setopt_array($curl, array(
+//             CURLOPT_URL => "https://hoqustaging.webmapp.it/api/store",
+//             CURLOPT_RETURNTRANSFER => true,
+//             CURLOPT_ENCODING => "",
+//             CURLOPT_MAXREDIRS => 10,
+//             CURLOPT_TIMEOUT => 30,
+//             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+//             CURLOPT_CUSTOMREQUEST => "POST",
+//             CURLOPT_POSTFIELDS => $CURLOPT_POSTFIELDS_ARRAY,
+//             CURLOPT_HTTPHEADER => array(
+//               "Accept: application/json",
+//               "Content-type: application/json",
+//               "Authorization : Bearer $hoqu_token"
+//             ),
+//           ));
         
-          $response = curl_exec($curl);
-          $err = curl_error($curl);
+//           $response = curl_exec($curl);
+//           $err = curl_error($curl);
           
-          print_r($response);
-          curl_close($curl);
-    }
+//           print_r($response);
+//           curl_close($curl);
+//     }
     
-}
+// }
