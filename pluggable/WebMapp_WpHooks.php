91d0fdbeb0868ead4fa5b2c1232723058bd13868
Pedram Katanchi <pedramkat@gmail.com>
2020-11-04 13:11:16 +0100

    modified pdf-large image size

--- pluggable/WebMapp_WpHooks.php
+++ pluggable/WebMapp_WpHooks.php
@@ -378,6 +378,6 @@ function wm_copy_featured_image_on_save_poi ( $post_id, $post, $update ){
 
 add_action( 'after_setup_theme', 'wpdocs_theme_setup' );
 function wpdocs_theme_setup() {
-    add_image_size( 'pdf-large', 1080, 608, array( 'center', 'center' ) ); // Hard crop left top
+    add_image_size( 'pdf-large', 1080, 608, array( 'center', 'center' ) ); // Hard crop center
 
 }
