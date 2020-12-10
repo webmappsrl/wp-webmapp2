7834e617f33be29911dccfb226ef47d9763d0fbc
Pedram Katanchi <pedramkat@gmail.com>
2020-12-10 22:01:26 +0100

    Added the lang param to wp wm-save-routes

--- pluggable/duplicable/wp-cli/WebMapp_wm-save-routes.php
+++ pluggable/duplicable/wp-cli/WebMapp_wm-save-routes.php
@@ -6,20 +6,29 @@
 # Success: bar qux
 
 /**
- * Updates all the custom post types ROUTE
+ * Updates all the custom post types ROUTE, for routes in a specific language type the language code as the first parameter (ex. en)
  *
  *
  * @when after_wp_load
  */
 $wm_save_routes = function( $args, $assoc_args )
 {
-    $results = new WP_Query( array( 'post_type' => 'route','post_status' => 'publish', 'posts_per_page' => -1) );
-    
+    global $sitepress;
+    $default_lang = $sitepress->get_default_language();
+    $lang = '';
+    if ( !empty($args)) {
+        $sitepress->switch_lang($args[0]);
+    }
+
+    $results = new WP_Query( array( 'post_type' => 'route','post_status' => 'publish','suppress_filters' => false, 'posts_per_page' => -1) );
+            
             foreach ( $results->posts as $post ) {
                 WP_CLI::success('Updating Route ID # ' . $post->ID);
                 wp_update_post( $post );
             }
 
+
+    $sitepress->switch_lang( $default_lang);
 
 
 };
